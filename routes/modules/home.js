c6ae300a679997e356ff192acd04a9dbc9095519
DESKTOP-VBC15AG\Administrator <catcolor2020@gmail.com>
2021-11-08 02:30:02 +0800

    feat: add search sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -33,13 +33,21 @@ router.get('/search', (req, res) => {
 
   const keyword = req.query.keyword.trim().toLowerCase()
   const keywords = req.query.keyword
+  const { name, sort } = req.query
 
-  if (!keywords) {
-    res.redirect('/')
+  let sortOrder = { _id: 'asc' }
+
+  if (name === 'name') {
+    sortOrder = { name: `${sort}` }
+  } else if (name === 'category') {
+    sortOrder = { category: `${sort}` }
+  } else if (name === 'location') {
+    sortOrder = { location: `${sort}` }
   }
 
   return Restaurant.find()
     .lean()
+    .sort(sortOrder)
     .then(restaurants => {
       const filterrestaurants = restaurants.filter(data => data.name.toLowerCase().includes(keyword) ||
         data.category.toLowerCase().includes(keyword))
